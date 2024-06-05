eb42a25dcb8eadf3e26943938ad76820f5a3ade8
Jay Bhullar <jaybhullar@Jays-MacBook-Pro.local>
2024-06-04 17:38:14 -0700

    added UI

---
+++ MockupFrontEnd/MockupFrontEnd/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Welcome to BookBazaar</h1>
+      <p>Your one-stop shop for all your book needs!</p>
+    </div>
+  );
+}
+
+export default Home;
