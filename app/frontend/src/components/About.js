2ab5a9754b3a108ef89632808e01ff18b03906d1
Nickouwen <71842732+Nickouwen@users.noreply.github.com>
2024-06-12 00:57:43 -0700

    Added auth checks to about and contact pages
    Might be a better way to make this into a hook. I tried for a little bit but could not figure it out right now

--- app/frontend/src/components/About.js
+++ app/frontend/src/components/About.js
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import useCheckSession from '../hooks/checkSession';
+
 
 const About = () => {
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+  const session = useCheckSession();
+  
+  useEffect(() => {
+    if (session === null) {
+     
+      setLoading(true);
+    } else if (session === false) {
+     
+      navigate("/login");
+    } else {
+     
+      setLoading(false);
+    }
+  }, [session, navigate]);
+
+  if(loading) {
+    return <div>Loading...</div>
+  }
   return (
     <div className="about p-8 bg-white shadow rounded w-3/5">
       <h1 className="text-4xl font-bold text-blue-900">About Us</h1>
