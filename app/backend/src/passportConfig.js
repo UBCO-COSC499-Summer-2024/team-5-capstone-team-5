e81399b70d4aec757d620e808e956b94b78e6cb8
Jay0808 <jaideepsinghbhullar@gmail.com>
2024-06-13 21:32:27 -0700

    Login page updated with authentication

---
+++ app/backend/src/passportConfig.js
@@ -0,0 +1,42 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+const { db } = require('./database');
+
+passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+  },
+  async (email, password, done) => {
+    try {
+      const user = await db.oneOrNone("SELECT * FROM users WHERE email = $1", [email]);
+      if (!user) {
+        return done(null, false, { message: 'Incorrect email.' });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+
+      return done(null, user);
+    } catch (error) {
+      return done(error);
+    }
+  }
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await db.oneOrNone("SELECT * FROM users WHERE id = $1", [id]);
+    done(null, user);
+  } catch (error) {
+    done(error, null);
+  }
+});
+
+module.exports = passport;
