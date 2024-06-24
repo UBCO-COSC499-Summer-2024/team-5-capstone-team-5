0ef418b057bad3f6425f20dc5789f6723314e0b5
Jay0808 <jaideepsinghbhullar@gmail.com>
2024-06-23 20:51:02 -0700

    Changes

--- app/frontend/src/components/Instructor/InstNavbar.js
+++ app/frontend/src/components/Instructor/InstNavbar.js
@@ -3,8 +3,8 @@ import { NavLink, useNavigate } from 'react-router-dom';
 import getCourseData from '../../hooks/getCourseData';
 import validateUser from '../../hooks/validateUser';
 import AddCourseModal from './AddCourseModal';
-import ProfileMenuModal from './ProfileMenuModal';
-import { useTheme } from '../../App';
+import ProfileMenuModal from './ProfileMenuModal'; // Ensure the path is correct
+import { useTheme } from '../../App'; // Adjust the path as needed
 
 const InstNavbar = (props) => {
   const [courses, setCourses] = useState([]);
@@ -15,7 +15,7 @@ const InstNavbar = (props) => {
   const [user, setUser] = useState({
     name: 'John Doe',
     email: 'johndoe@example.com',
-    image: 'https://via.placeholder.com/150',
+    image: 'https://via.placeholder.com/150', // Replace with actual user image URL
   });
 
   useEffect(() => {
@@ -25,6 +25,7 @@ const InstNavbar = (props) => {
         navigate("/login");
       }
     };
+
     checkSession();
   }, [navigate]);
 
@@ -37,11 +38,11 @@ const InstNavbar = (props) => {
     try {
       if (props.id) {
         const courseData = await getCourseData(props.id);
-        setCourses(courseData || []);
+        setCourses(courseData || []); // Ensure courses is always an array
       }
     } catch (error) {
       console.error('Error fetching courses:', error);
-      setCourses([]);
+      setCourses([]); // Fallback to an empty array in case of an error
     }
   };
 
@@ -50,12 +51,12 @@ const InstNavbar = (props) => {
   }, [props.id]);
 
   const handleAddCourse = async (data) => {
-    console.log("data: ",data)
-    const name = data.courseDept + " " + data.courseCode + "-" + data.courseSection
-    const description = data.description
-    const endDate = data.endDate
-    const newCourse = {name: name, description: description, end_date: endDate, user_id: props.id}
-    console.log("newCourse: ",newCourse);
+    console.log("data: ", data);
+    const name = data.courseDept + " " + data.courseCode + "-" + data.courseSection;
+    const description = data.description;
+    const endDate = data.endDate;
+    const newCourse = { name: name, description: description, end_date: endDate, user_id: props.id };
+    console.log("newCourse: ", newCourse);
     try {
       const response = await fetch('http://localhost/api/users/courses/add', {
         method: 'POST',
@@ -66,7 +67,7 @@ const InstNavbar = (props) => {
       });
       if (response.ok) {
         const addedCourse = await response.json();
-        console.log("addedCourse:",addedCourse)
+        console.log("addedCourse:", addedCourse);
         setCourses([...courses, addedCourse]);
       } else {
         console.error('Error adding course:', response.status, response.statusText);
@@ -87,14 +88,14 @@ const InstNavbar = (props) => {
               className={({ isActive }) =>
                 isActive
                   ? `block mt-4 mx-4 py-2 px-4 mb-2 rounded-lg ${theme === 'dark' ? 'bg-gray-700 text-white' : 'bg-gray-400 text-black'} font-bold`
-                  : `block mt-4 mx-4 py-2 px-4 mb-2 rounded-lg ${theme === 'dark' ? 'text-gray-300 hover:bg-gray-600 hover:text-white' : 'text-gray-800 hover:bg-gray-500 hover:text-black'}`
+                  : `block mt-4 mx-4 py-2 px-4 mb-2 rounded-lg ${theme === 'dark' ? 'text-gray-300 hover:bg-gray-600 hover:text-white' : 'text-black hover:bg-gray-400 hover:text-black'}`
               }
             >
               Recent Courses
             </NavLink>
           </div>
           <div className="mt-4">
-            <h2 className={`ml-4 text-lg font-bold ${theme === 'dark' ? 'text-gray-300' : 'text-gray-800'}`}>Courses</h2>
+            <h2 className="ml-4 text-lg font-bold text-gray-300">Courses</h2>
             <ul className="mt-4 space-y-4">
               {courses.length > 0 ? courses.map((course) => (
                 <li key={course.course_id}>
@@ -102,23 +103,26 @@ const InstNavbar = (props) => {
                     to={`/instructor/course/${course.course_id}`}
                     className={({ isActive }) =>
                       isActive
-                        ? `block ${theme === 'dark' ? 'bg-gray-700' : 'bg-gray-400'} p-4 mx-4 rounded-lg hover:bg-gray-600`
-                        : `block ${theme === 'dark' ? 'bg-gray-800' : 'bg-gray-300'} p-4 mx-4 rounded-lg hover:bg-gray-600`
+                        ? `block p-4 mx-4 rounded-lg ${theme === 'dark' ? 'bg-gray-700 hover:bg-gray-600' : 'bg-gray-300 hover:bg-gray-400'}`
+                        : `block p-4 mx-4 rounded-lg ${theme === 'dark' ? 'bg-gray-800 hover:bg-gray-600' : 'bg-gray-200 hover:bg-gray-300'}`
                     }
                   >
-                    <h3 className={`${theme === 'dark' ? 'text-white' : 'text-black'} font-bold`}>{course.name}</h3>
-                    <p className={`${theme === 'dark' ? 'text-gray-400' : 'text-gray-700'}`}>{course.description}</p>
-                    <p className={`${theme === 'dark' ? 'text-gray-500' : 'text-gray-600'}`}>Ends: {course.end_date.slice(0, 10)}</p>
+                    <h3 className="font-bold">{course.name}</h3>
+                    <p className={`text-gray-400`}>{course.description}</p>
+                    <p className="text-gray-500">Ends: {course.end_date.slice(0, 10)}</p>
                   </NavLink>
                 </li>
               )) : (
                 <li className="text-center text-gray-400">No courses available</li>
               )}
               <li
-                className={`block ${theme === 'dark' ? 'bg-gray-800' : 'bg-gray-300'} p-4 mx-4 rounded-lg hover:bg-gray-600 text-white font-bold text-center cursor-pointer`}
+                className={`block p-4 mx-4 rounded-lg hover:bg-gray-600 text-white font-bold text-center cursor-pointer ${theme === 'dark' ? 'bg-gray-800' : 'bg-gray-300'} relative`}
                 onClick={() => setIsModalOpen(true)}
               >
                 <div className="text-xl">+</div>
+                <div className="tooltip absolute bottom-[-1.5rem] left-1/2 transform -translate-x-1/2 bg-black text-white p-1 rounded opacity-0 transition-opacity duration-300">
+                  Add Course
+                </div>
               </li>
             </ul>
           </div>
@@ -129,8 +133,8 @@ const InstNavbar = (props) => {
           to="/about"
           className={({ isActive }) =>
             isActive
-              ? `block py-2 px-4 mb-2 rounded-lg ${theme === 'dark' ? 'bg-gray-700 text-white font-bold' : 'bg-gray-400 text-black font-bold'}`
-              : `block py-2 px-4 mb-2 rounded-lg ${theme === 'dark' ? 'text-gray-300 hover:bg-gray-700 hover:text-white' : 'text-gray-800 hover:bg-gray-500 hover:text-black'}`
+              ? `block py-2 px-4 mb-2 rounded-lg ${theme === 'dark' ? 'bg-gray-700 text-white' : 'bg-gray-400 text-black'} font-bold`
+              : `block py-2 px-4 mb-2 rounded-lg ${theme === 'dark' ? 'text-gray-300 hover:bg-gray-700 hover:text-white' : 'text-black hover:bg-gray-400 hover:text-black'}`
           }
         >
           About
@@ -139,20 +143,20 @@ const InstNavbar = (props) => {
           to="/contact"
           className={({ isActive }) =>
             isActive
-              ? `block py-2 px-4 mb-2 rounded-lg ${theme === 'dark' ? 'bg-gray-700 text-white font-bold' : 'bg-gray-400 text-black font-bold'}`
-              : `block py-2 px-4 mb-2 rounded-lg ${theme === 'dark' ? 'text-gray-300 hover:bg-gray-700 hover:text-white' : 'text-gray-800 hover:bg-gray-500 hover:text-black'}`
+              ? `block py-2 px-4 mb-2 rounded-lg ${theme === 'dark' ? 'bg-gray-700 text-white' : 'bg-gray-400 text-black'} font-bold`
+              : `block py-2 px-4 mb-2 rounded-lg ${theme === 'dark' ? 'text-gray-300 hover:bg-gray-700 hover:text-white' : 'text-black hover:bg-gray-400 hover:text-black'}`
           }
         >
           Contact
         </NavLink>
         <button
           onClick={() => setIsProfileMenuOpen(true)}
-          className={`flex items-center mb-4 cursor-pointer ${theme === 'dark' ? 'hover:bg-gray-700' : 'hover:bg-gray-500'} p-2 rounded-lg`}
+          className={`flex items-center mb-4 cursor-pointer hover:bg-gray-700 p-2 rounded-lg ${theme === 'dark' ? 'bg-gray-800 text-white' : 'bg-gray-200 text-black'}`}
         >
           <img src={user.image} alt="User" className="w-10 h-10 rounded-full mr-4" />
           <div>
-            <p className={`${theme === 'dark' ? 'text-sm font-bold text-white' : 'text-sm font-bold text-black'}`}>{user.name}</p>
-            <p className={`${theme === 'dark' ? 'text-xs text-gray-400' : 'text-xs text-gray-600'}`}>{user.email}</p>
+            <p className="text-sm font-bold">{user.name}</p>
+            <p className={`text-xs ${theme === 'dark' ? 'text-gray-400' : 'text-gray-600'}`}>{user.email}</p>
           </div>
         </button>
         <ProfileMenuModal
