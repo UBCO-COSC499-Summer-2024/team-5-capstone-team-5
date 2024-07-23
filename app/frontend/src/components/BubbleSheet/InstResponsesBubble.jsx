24ccc38f1d7e3355fd704a89495d8f08a8e4b54a
OakleyPankratz <oakleyapankratz@gmail.com>
2024-07-22 22:49:02 -0700

    InstResponseBubbles are now responsive
    Bubbles now change their colour when clicked by the instructor as to edit the students response. TODO: send these changes to the database when the instructor clicks "save", style the "save" button, highlight questions which have been edited/modified.

--- app/frontend/src/components/BubbleSheet/InstResponsesBubble.jsx
+++ app/frontend/src/components/BubbleSheet/InstResponsesBubble.jsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 
 let modifiedResponses = [];
+//(exam_id, question_num, user_id, response, modifying = false)
 
 function Bubble(props) {
+  const isCorrect = props.isCorrect;
+  const [isSelected, setIsSelected] = useState(props.isSelected);
+  const onSelect = (option) => {
+    console.log(option);
+    setIsSelected(!isSelected);
+  };
+  let color = "bg-gray-700";
+      if (isCorrect) {
+        if (isSelected) {
+          color = "bg-green-500";
+        } else {
+          color = "bg-yellow-600";
+        }
+      } else if (isSelected) {
+        color = "bg-red-700";
+      }
   return (
     <div
       key={props.option}
-      className={`w-8 h-8 flex items-center justify-center rounded-full border cursor-pointe
-         ${props.color} text-white mx-1 my-0.5`}
+      className={`w-8 h-8 flex items-center justify-center rounded-full border cursor-pointer
+         ${color} text-white mx-1 my-0.5`}
       onClick={() => {
-        props.onSelect(props.option);
+        onSelect(props.option);
+
       }}
     >
       <p>{String.fromCharCode(props.option + 65)}</p>
@@ -18,40 +36,32 @@ function Bubble(props) {
 }
 
 function CreateBubbles(props) {
+  //const [modified, setModified] = useState(false);
   const responseItem = props.responseItem;
-  const onSelect = (option) => {
-    console.log(option);
-  };
-  if (typeof responseItem !== "undefined") {
+  if (typeof props.responseItem !== "undefined") {
     let bubbles = [];
-    console.log(responseItem);
     for (let i = 0; i < responseItem.num_options; i++) {
-      let color = "bg-gray-700";
-      if (responseItem.correct_answer.includes(i)) {
-        if (responseItem.response.includes(i)) {
-          color = "bg-green-500";
-        } else {
-          color = "bg-yellow-600";
-        }
-      } else if (responseItem.response.includes(i)) {
-        color = "bg-red-700";
-      }
+      let isSelected = responseItem.response.includes(i);
+      let isCorrect = responseItem.correct_answer.includes(i)
       bubbles.push(
         <Bubble
           key={i}
           option={i}
           letter={String.fromCharCode(i + 65)}
-          onSelect={onSelect}
-          color={color}
+          isSelected = {isSelected}
+          isCorrect = {isCorrect}
         />
       );
     }
+    //setModified(true);
+    let modified = false;
+    let textColor = modified ? "text-yellow-600" : "text-white";
     return (
       <div
         key={responseItem.question_num}
         className="bg-gray-900 flex space-x-2 flex-right p-[2px] px-[5px] w-auto mb-3"
       >
-        <p className="w-[105px] mt-[5px]">
+        <p className={`w-[105px] mt-[5px] ${textColor}`}>
           Question: {String(responseItem.question_num)}
         </p>
         {bubbles}
@@ -64,7 +74,7 @@ function CreateBubbles(props) {
 function InstResponseBubbles(props) {
   const responseData = props.responseData;
   if (typeof props.responseData !== "undefined") {
-    console.log(responseData[0].userId);
+    console.log(responseData);
     return (
       <>
         <div
